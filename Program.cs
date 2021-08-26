82e3a91b08599ad7f52bb3dfad6b08f8c4d71bf7
acondic <89468047+acondic@users.noreply.github.com>
2021-08-26 18:43:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BA1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PatternCount(string text,string pattern)
+            {
+                int count = 0;
+                for(int i = 0; i < text.Length - pattern.Length; i++)
+                {
+                    if (text.Substring(i, pattern.Length) == pattern)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            string a = "TAACAGCCTTTAGCCTTTAGCCTTTAGCCTTTAGCCTTTAGCCTTTGAGCCTTTGAGCCTTTTAGCCTTTCAGCCTT\nCCT";
+            string[] inlines = a.Split("\n");
+            string text = inlines[0];
+            string pattern = inlines[1];
+
+            int broj = PatternCount(text, pattern);
+            Console.WriteLine("Broj ponavljanja je:" + broj);
+        }
+    }
+}
