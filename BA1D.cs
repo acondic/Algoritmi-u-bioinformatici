b67a08065bb8fd81ca09c7648ceeb58f7cfe4bf5
acondic <89468047+acondic@users.noreply.github.com>
2021-08-26 18:49:28 +0200

    Add files via upload

---
+++ BA1D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BA1D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p = "GTC";
+            string g = "ACTGGTCACCGTAGTCAA";
+            List<int> lista = new List<int>();
+            for(int i = 0; i < g.Length - p.Length; i++)
+            {
+                if (g.Substring(i, p.Length) == p)
+                {
+                    lista.Add(i);
+                }
+            }
+
+            for(int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i] + " ");
+            }
+        }
+    }
+}
